186de2fac40587479c2b7d3a9c9142fed6d9ce4f
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-07-19 22:57:10 +0530

    email class

---
+++ ShineWay/Classes/Emails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShineWay.Classes
+{
+    class Emails
+    {
+        public static void sendEmail(String to, String subject, string messageBody)
+        {
+            MailMessage message = new MailMessage("ShineWayRentalLk@gmail.com", to);
+            message.Subject = subject;
+            message.Body = messageBody;
+            SmtpClient client = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("ShineWayRentalLk@gmail.com", "shineway@123"),
+                EnableSsl = true,
+            };
+
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in CreateTestMessage2(): {0}",
+                    ex.ToString());
+            }
+
+        }
+    }
+}
