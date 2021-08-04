9e47c8dbe7f6e69514520baee917839fa4d944e8
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-08-04 15:12:00 +0530

    users set 1st row details when load

--- ShineWay/UI/Users.cs
+++ ShineWay/UI/Users.cs
@@ -475,6 +475,35 @@ namespace ShineWay.UI
             dataGridView1.RowHeadersVisible = false;
             dataGridView1.ColumnHeadersDefaultCellStyle.SelectionBackColor = dataGridView1.ColumnHeadersDefaultCellStyle.BackColor;
 
+
+            //to set the already selected row details to fields
+            isNameValid = true;
+            isNICValid = true;
+            isTelephoneNumberValid = true;
+            isAddressValid = true;
+            isEmailValid = true;
+            txt_NIC.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            txt_name.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txt_telephoneNumber.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txt_email.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+            txt_address.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+            combo_userType.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            //to reset warnings
+            pictureBox7.Image = ShineWay.Properties.Resources.correctInput;
+            pictureBox5.Image = ShineWay.Properties.Resources.correctInput;
+            label_nicError.Visible = false;
+            pictureBox9.Image = ShineWay.Properties.Resources.correctInput;
+            label_telError.Visible = false;
+            pictureBox11.Image = ShineWay.Properties.Resources.correctInput;
+            pictureBox3.Image = ShineWay.Properties.Resources.correctInput;
+            label_addressError.Visible = false;
+            label_telTick.Visible = false;
+            label_tickAddress.Visible = false;
+            label_tickNIC.Visible = false;
+            label_tickName.Visible = false;
+            label_tickEmail.Visible = false;
+            label_emailError.Visible = false;
+            label_nameError.Visible = false;
         }
 
 
