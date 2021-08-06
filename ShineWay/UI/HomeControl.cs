97e023ba9ca9cd618d7eb72051e4234d6faedb25
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-08-06 14:59:34 +0530

    home controll image error resolved

--- ShineWay/UI/HomeControl.cs
+++ ShineWay/UI/HomeControl.cs
@@ -43,7 +43,7 @@ namespace ShineWay.UI
 
                 try
                 {
-                    pb_vehicle.Image = Image.FromFile(@"C:\ShineWay\img\" + vehicles[vehicleIndex].getVehicleNumber() + ".jpg");
+                    pb_vehicle.Image = Image.FromFile(@"C:\ShineWay\img\" + vehicles[vehicleIndex].getVehicleNumber() + "-overall.jpg");
                 }
                 catch (Exception ex)
                 {
@@ -90,7 +90,7 @@ namespace ShineWay.UI
                 label_weeklyRental.Text = vehicles[vehicleIndex].getWeeklyRental();
                 try
                 {
-                    pb_vehicle.Image = Image.FromFile(@"C:\ShineWay\img\" + vehicles[vehicleIndex].getVehicleNumber() + ".jpg");
+                    pb_vehicle.Image = Image.FromFile(@"C:\ShineWay\img\" + vehicles[vehicleIndex].getVehicleNumber() + "-overall.jpg");
                 }
                 catch (Exception ex)
                 {
