fa827e38c11035327188a0fb4f69ecfd5d16404b
SajeeRodrigo <sajee.thamanga@gmail.com>
2021-08-06 16:48:33 +0530

    vehicle, vehicle owner, owner payment, customer

--- ShineWay/Properties/Resources.Designer.cs
+++ ShineWay/Properties/Resources.Designer.cs
@@ -253,6 +253,16 @@ namespace ShineWay.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ErrorInputVehicle {
+            get {
+                object obj = ResourceManager.GetObject("ErrorInputVehicle", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap eye {
             get {
                 object obj = ResourceManager.GetObject("eye", resourceCulture);
