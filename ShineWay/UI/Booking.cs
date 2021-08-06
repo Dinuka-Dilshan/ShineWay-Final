0c5f2615790160c999f6923e2495ca0eb0d2b1b6
Avishka Rodrigo <avish.rodrigo11@gmail.com>
2021-08-06 22:42:21 +0530

    data repeat resolved in datagrid view

--- ShineWay/UI/Booking.cs
+++ ShineWay/UI/Booking.cs
@@ -362,7 +362,7 @@ namespace ShineWay.UI
             if (txt_search.Text != "") {
                 try
                 {
-                    setDataToGrid("SELECT `booking`.`Vehicle_num`, `booking`.`Booking_ID`,`booking`.`Licen_num`,`booking`.`Cus_NIC`, `Start_date`, `Start_ODO`,`End_date`,`Package_Type`,`Deposite_Amount`,`Advance_Payment`,`Discription`  FROM `booking`,`payment` WHERE `booking`.`Vehicle_num` LIKE '%" + txt_search.Text + "%' OR `booking`.`Booking_ID` LIKE '%" + txt_search.Text + "%' OR `booking`.`Cus_nic` LIKE '%" + txt_search.Text + "%' OR `booking`.`Licen_num` LIKE '%" + txt_search.Text + "%' OR `Start_date` LIKE '%" + txt_search.Text + "%' OR `Start_ODO` LIKE '%" + txt_search.Text + "%'");
+                    setDataToGrid("SELECT `booking`.`Vehicle_num`, `booking`.`Booking_ID`,`booking`.`Licen_num`,`booking`.`Cus_NIC`, `Start_date`, `Start_ODO`,`End_date`,`Package_Type`,`Deposite_Amount`,`Advance_Payment`,`Discription`  FROM `booking`,`payment` WHERE `booking`.`Vehicle_num` LIKE '%" + txt_search.Text + "%' OR `booking`.`Booking_ID` LIKE '%" + txt_search.Text + "%' OR `booking`.`Cus_nic` LIKE '%" + txt_search.Text + "%' OR `booking`.`Licen_num` LIKE '%" + txt_search.Text + "%' OR `Start_date` LIKE '%" + txt_search.Text + "%' OR `Start_ODO` LIKE '%" + txt_search.Text + "%' GROUP BY booking.Booking_ID");
                 }
                 catch (Exception exsearch)
                 {
